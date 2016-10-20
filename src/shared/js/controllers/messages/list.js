237a2c04515633657b7070457bd2eaadccbc80e5
anton <anton.koch@gmail.com>
2016-10-21 01:40:22 +0200

    flipped messages controller functions

--- src/shared/js/controllers/messages/list.js
+++ src/shared/js/controllers/messages/list.js
@@ -1,32 +1,52 @@
 'use strict';
 
-var async = require('async');
-
-var MessagesListController = function ($scope, $q, $location, apiService, responseHandler) {
-    var deferred = $q.defer();
-    $scope.promise = deferred.promise;
-    $scope.message = {
-        recipient_uuid: null,
-        body: null
-    };
-    async.waterfall([
-        function (cb) {
-            if ($location.search().recipient_uuid) {
-                apiService('users').actions.find($location.search().recipient_uuid, cb);
-            } else {
-                cb(new Error('errors.unknown'));
-            }
+var MessagesListController = function ($scope, apiService, responseHandler, $q) {
+    $scope.fields = [
+        {
+            label: 'messages.message',
+            property: 'body',
+            sort: false
         },
-        function (user, cb) {
-            $scope.recipient = user;
-            $scope.message.recipient_uuid = user.uuid;
-            cb();
+        {
+            label: 'messages.from',
+            property: 'recipient.nickname',
+            sort: false
+        },
+        {
+            label: 'messages.sent',
+            property: 'created',
+            date: true,
+            sort: false
         }
-    ], function (err) {
-        responseHandler.handleResponse(err, deferred);
-    });
+    ];
+    $scope.settings = {
+        row_select: false,
+        multiple: false,
+        pagination: true,
+        pagesize: 25,
+        limit_options: [25, 50, 100],
+        resource: 'users/me/messages'
+    };
+    $scope.actions = [
+        {
+            label: 'actions.delete',
+            css_class: 'fa-trash-o',
+            clickHandler: function (message) {
+                var deferred = $q.defer();
+                $scope.promise = deferred.promise;
+                apiService('locations').actions.remove(message.uuid, function (err) {
+                    var msgs = {
+                        success: 'messages.remove_success'
+                    };
+                    if (responseHandler.handleResponse(err, deferred, msgs)) {
+                        window.document.location.reload();
+                    }
+                });
+            }
+        }
+    ];
 };
 
-MessagesListController.$inject = ['$scope', '$q', '$location', 'apiService', 'responseHandler'];
+MessagesListController.$inject = ['$scope', 'apiService', 'responseHandler', '$q'];
 
 module.exports = MessagesListController;
