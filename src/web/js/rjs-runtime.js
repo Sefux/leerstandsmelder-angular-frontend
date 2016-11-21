e4620507d43c58b9c442ef1a35228267eb22bed5
anton <anton.koch@gmail.com>
2016-11-21 12:29:05 +0100

    updated build number

--- src/web/js/rjs-runtime.js
+++ src/web/js/rjs-runtime.js
@@ -4,7 +4,7 @@
     'use strict';
 
     require.config({
-        urlArgs: "build=16061501",
+        urlArgs: "build=16112101",
         paths: {
             lang_en: '/src/shared/js/lang/en',
             lang_de: '/src/shared/js/lang/de',
