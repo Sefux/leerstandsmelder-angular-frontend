36afdd5546948f7dfb8d7f887350b5e6cd570f8e
Sebastian Fuchs <sfuchs@projektkater.de>
2017-10-19 03:11:52 +0200

    Fix view locations per region

--- src/shared/js/controllers/locations/region-list.js
+++ src/shared/js/controllers/locations/region-list.js
@@ -1,87 +1,109 @@
 'use strict';
 
+var async = require('async');
+
 var LocationsRegionListController = function ($scope, $q, $location, $mdDialog, $translate, responseHandler,
-                                              apiService, regionService, $routeParams) {
-    var fields = [
-        {
-            label: 'locations.title',
-            property: 'title'
+                                              apiService, regionService, $routeParams, configuration, $filter) {
+    
+    var deferred = $q.defer();
+    $scope.promise = deferred.promise;
+    $scope.urlbase = configuration.urlbase || '/';
+                                                  
+    var columnDefs = [
+        {headerName: $filter('translate')("locations.title"), field: "title", width: 120, sort: 'asc' },
+        {headerName: $filter('translate')("locations.street"), field: "street", width: 90},
+        {headerName: $filter('translate')("author.created"), field: "created", width: 90, cellRenderer: dateFormatter},
+        {headerName: $filter('translate')("author.updated"), field: "updated", width: 90, cellRenderer: dateFormatter},
+        {headerName: $filter('translate')("locations.hidden"), field: "hidden", width: 90, cellRenderer: booleanFormatter }, //
+        {headerName: "", field: "uuid", width: 60, suppressFilter: true, cellRenderer: function (params) {      // Function cell renderer
+           return '<a class="md-icon-button md-table-button md-raised  md-fab  md-mini " href="' + $scope.urlbase + (params.data.region ? params.data.region.slug : params.data.region_uuid)+ '/' + (params.data.slug || params.data.uuid) + '" aria-label="' + $filter('translate')("actions.view") + '"><md-icon md-font-icon="fa-eye" class="fa fa-eye"></md-icon></a>';
+        }
         },
-        {
-            label: 'locations.street',
-            property: 'street'
+        {headerName: "", field: "uuid", width: 60, suppressFilter: true, cellRenderer: function (params) {      // Function cell renderer
+           return '<a class="md-icon-button md-table-button md-raised  md-fab  md-mini " href="' + $scope.urlbase +  'locations/update/' + (params.data.slug || params.data.uuid) + '" aria-label="' + $filter('translate')("actions.edit") + '"><md-icon md-font-icon="fa-pencil" class="fa fa-pencil"></md-icon></a>';
+        }
         },
-        {
-            label: 'author.created',
-            property: 'created',
-            sort: true,
-            date: true
-        },
-        {
-            label: 'author.updated',
-            property: 'updated',
-            date: true
-        },
-        {
-            label: '',
-            property: 'edit'
-        },
-        {
-            label: '',
-            property: 'show'
-        },
-        {
-            label: '',
-            property: 'delete'
+        {headerName: "", field: "uuid", width: 60, suppressFilter: true, cellRenderer: function (params) {      // Function cell renderer
+           return '<a class="md-icon-button md-table-button md-raised  md-fab  md-mini " ng-click="clickDeleteHandler(\'' + params.value + '\',' + params.rowIndex + ')" aria-label="' + $filter('translate')("actions.delete") + '"><md-icon md-font-icon="fa-trash" class="fa fa-trash"></md-icon></a>';
+        }
         }
     ];
-    var rowSetup = {
-        'table-row-id-key': 'uuid',
-        'column-keys': [
-            'title',
-            'street',
-            'created',
-            'updated',
-
-        ],
-    };
+    
     if ($routeParams.region_uuid) {
-        fields.push({
-            label: 'locations.hidden',
-            property: 'hidden'
-        });
-        rowSetup['column-keys'].push('hidden');
         $scope.listHeadline = 'locations.locations_by_region';
     } else {
         $scope.listHeadline = 'locations.my_locations';
     }
-    //TODO: check permitions
-    rowSetup['column-keys'].push('edit');
-    rowSetup['column-keys'].push('show');
+    
+    $scope.gridOptions = {
+        columnDefs: columnDefs,
+        rowData: null,
+        rowHeight: 58,
+        rowClassRules: {
+            'hidden-warning': 'data.hidden'
+        },
+        enableSorting: true,
+        enableFilter: true,
+        enableColResize: true,
+        animateRows: true,
+        angularCompileRows: true,
+        getRowNodeId: function(data) { return data.uuid; },
+        onGridReady: function() {
+            setTimeout(function() {
+                $scope.gridOptions.api.sizeColumnsToFit();
+            }, 600);
+        }
+    };
 
-    rowSetup['column-keys'].push('delete');
+    function dateFormatter(params) {
+        return $filter('date')(params.value,'yyyy-MM-dd');
+    }
+    
+    function booleanFormatter(params) {
+        return (params.value ? $filter('translate')("generel.yes"):$filter('translate')("generel.no"));
+    }
+
+    $scope.filterGrid = function() {
+        $scope.gridOptions.api.setQuickFilter($scope.filterStr);
+    };
     regionService.setCurrentRegion($routeParams.region_uuid, function () {
         $scope.currentRegion = regionService.currentRegion.title;
     });
-    $scope.fields = fields;
-    $scope.settings = {
-        row_select: false,
-        multiple: false,
-        pagination: true,
-        sort: '-created',
-        pagesize: 25,
-        limit_options: [25, 50, 100],
-        resource: $routeParams.region_uuid ? 'regions/' + $routeParams.region_uuid + '/locations' : 'users/me/locations'
-    };
-    $scope.rowSetup = rowSetup;
-
-    $scope.clickShowHandler =  function (uuid) {
-        $location.path('/location/' + uuid);
-    };
-    $scope.clickEditHandler = function (uuid) {
-        $location.path('/admin/regions/' + uuid);
-    };
-    $scope.clickDeleteHandler = function (uuid) {
+    
+    async.waterfall([
+        function (cb) {
+            if ($routeParams.region_uuid) {
+                apiService('regions').actions.find($routeParams.region_uuid, cb);
+            } else {
+                apiService('regions?sort=title').actions.all(cb);
+            }
+        },
+        function (regions, cb) {
+            var region_data = regions.results || regions;
+            if (Array.isArray(region_data)) {
+                $scope.data = region_data.results || region_data;
+                $scope.gridOptions.api.setRowData($scope.data);
+                $scope.gridOptions.api.sizeColumnsToFit();
+                cb();
+            } else {
+                $scope.region = region_data;
+                apiService('regions/' + region_data.uuid + '/locations').actions.all(cb);
+            }
+        }
+    ], function (err, locations) {
+        if (locations) {
+            $scope.listHeadline = $translate.instant('locations.locations_by_region') + ': ' +
+                ($scope.region ? $scope.region.title : null);
+            $scope.data = locations.results || locations;
+            $scope.gridOptions.api.setRowData($scope.data);
+            $scope.gridOptions.api.sizeColumnsToFit();
+        } else {
+            $scope.listHeadline = $translate.instant('locations.locations_by_region');
+        }
+        responseHandler.handleResponse(err, deferred);
+    });
+    
+    $scope.clickDeleteHandler = function (uuid, index) {
         var confirm = $mdDialog.confirm()
             .title($translate.instant('locations.remove_confirm_title'))
             .textContent($translate.instant('locations.remove_confirm_body'))
@@ -91,12 +113,13 @@ var LocationsRegionListController = function ($scope, $q, $location, $mdDialog, 
         $mdDialog.show(confirm).then(function () {
             var deferred = $q.defer();
             $scope.promise = deferred.promise;
+            
             apiService('locations').actions.remove(uuid, function (err) {
                 var msgs = {
                     success: 'locations.remove_success'
                 };
                 if (responseHandler.handleResponse(err, deferred, msgs)) {
-                    window.document.location.reload();
+                    var res = $scope.gridOptions.api.updateRowData({remove: [{'uuid':uuid}]});
                 }
             });
         });
@@ -104,6 +127,6 @@ var LocationsRegionListController = function ($scope, $q, $location, $mdDialog, 
 };
 
 LocationsRegionListController.$inject = ['$scope', '$q', '$location', '$mdDialog', '$translate', 'responseHandler',
-    'apiService', 'regionService', '$routeParams'];
+    'apiService', 'regionService', '$routeParams', 'configuration', '$filter'];
 
 module.exports = LocationsRegionListController;
