28fa8de32649e35b3ccf167dafe8c4afe821367e
Sebastian Fuchs <sfuchs@projektkater.de>
2018-10-25 16:44:31 +0200

    Change gallery layout

--- src/shared/js/filters/index.js
+++ src/shared/js/filters/index.js
@@ -3,3 +3,8 @@
 var app = require('angular').module('mapoz');
 
 app.filter('artworkSinceRewrite', require('./artwork-since-rewrite'));
+app.filter('artworkTypeClassName', function () {
+  return function (item) {
+    return item.substring(item.lastIndexOf(".") + 1);
+  };
+});
